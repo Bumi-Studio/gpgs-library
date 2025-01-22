f18c9333f763f296cc65ca89d2d7ba3ffcbccaac
thisisfatah <48764326+thisisfatah@users.noreply.github.com>
2025-01-22 16:20:22 +0700

    first commit

---
+++ Runtime/Scripts/TimeResponse.cs
@@ -0,0 +1,20 @@
+namespace Rto.Library
+{
+	[System.Serializable]
+	public class TimeResponse
+	{
+		public int year;
+		public int month;
+		public int day;
+		public int hour;
+		public int minute;
+		public int seconds;
+		public int milliseconds;
+		public string dateTime;
+		public string date;
+		public string time;
+		public string timeZone;
+		public string dayOfWeek;
+		public bool dstActive;
+	}
+}
